a5c9d959346942758da8c13a7aedd9e225d5ba54
Habiba <habebaosama14@gmail.com>
2022-03-18 23:48:33 +0200

    Initial commit

---
+++ src/RBTree.java
@@ -0,0 +1,12 @@
+public class RBTree {
+    private class RBNode{
+        private RBNode left;
+        private RBNode right;
+        private RBNode parent;
+        private int value;
+        private char color;
+        RBNode(int value){this.value=value;}
+
+    }
+    private RBNode root;
+}
