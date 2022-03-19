ecd40004eabca2aa343537c8884db4b02fc436a0
Habiba <habebaosama14@gmail.com>
2022-03-19 02:06:48 +0200

    before insert

--- src/RBTree.java
+++ src/RBTree.java
@@ -6,11 +6,68 @@ public class RBTree {
         private int value;
         private char color;
         RBNode(int value){this.value=value;}
+        public boolean isLeftChild(){return this==parent.left;}
+        public void setColor(char color) {
+            this.color = color;
+        }
+        public void switchColour(){
+            setColor(color=='R'? 'B':'R');
+        }
 
     }
+    /////////////////////////////////////////////////////////
     private RBNode root;
+
+
     public boolean isEmpty(){
         return root==null;
     }
+    public RBNode getRoot() {
+        return root;
+    }
+
+    public void clear(){
+        root=null;
+    }
+
+    public String search(int value){
+        var current=root;
+        while(current!=null){
+            if(value>current.value)
+                current=current.right;
+            else if(value< current.value)
+                current=current.left;
+            else
+                return current.value+" "+ +current.color;
+        }
+        return null;
+    }
+
+    public boolean contain(int value){
+        var current=root;
+        while(current!=null){
+            if(value>current.value)
+                current=current.right;
+            else if(value< current.value)
+                current=current.left;
+            else
+                return true;
+        }
+        return false;
+    }
+
+    //printing
+    public void traversePreOrder(){
+        traversePreOrder(root);
+    }
+    private void traversePreOrder(RBNode root){
+        if(root!=null) {
+            System.out.print (root.value +" " + "\'"+ root.color+"\'"+ " ");
+            traversePreOrder(root.left);
+            traversePreOrder(root.right);
+        }
+    }
+    ////////////////////////////////////
+
 
 }
