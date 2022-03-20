ce3cb230425d9d03350b552088780d4b15be6761
Habiba <habebaosama14@gmail.com>
2022-03-20 16:35:47 +0200

    insert  modification

--- src/RBMain.java
+++ src/RBMain.java
@@ -1,10 +1,11 @@
 public class RBMain {
     public static void main(String[] arg){
         RBTree tree=new RBTree();
-        tree.insert(8);
-        tree.insert(18);
-        tree.insert(5);
+        tree.insert(3);
+        tree.insert(21);
+        tree.insert(32);
         tree.insert(15);
-        tree.insert(17);
+
+        tree.traversePreOrder();
     }
 }
